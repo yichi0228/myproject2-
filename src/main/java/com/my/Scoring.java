fd1f3d719c41427dd7596762408b6b514e1c6e86
yi chi <a0968343489@gmail.com>
2022-03-04 16:09:58 +0800

    Scoring

---
+++ src/main/java/com/my/Scoring.java
@@ -0,0 +1,10 @@
+package com.my;
+
+public class Scoring {
+    public static void main(String[] args) {
+        Student jack=new Student();
+        jack.name="Jack";
+        jack.english =70;
+        jack.math=88;
+    }
+}
---
+++ src/main/java/com/my/Student.java
@@ -0,0 +1,7 @@
+package com.my;
+
+public class Student {
+    String  name;
+    int english;
+    int math;
+}
