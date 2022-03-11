f3745e62d26cbf743bbcd7278cdc121924f51ab9
yichi0228 <93140000+yichi0228@users.noreply.github.com>
2022-03-11 16:07:03 +0800

    Update Scoring.java

--- src/main/java/com/my/Scoring.java
+++ src/main/java/com/my/Scoring.java
@@ -1,10 +1,14 @@
 package com.my;
 
-public class Scoring {
-    public static void main(String[] args) {
-        Student jack=new Student();
-        jack.name="Jack";
-        jack.english =70;
-        jack.math=88;
+    public class Scoring {
+        public static void main(String[] args) {
+            Student jack=new Student("Jack",70,88);
+            Student hank= new Student("Hank",60,30);
+            GraduateStudent jane = new GraduateStudent();
+            jane.name="Jane";
+            jack.print();
+            hank.print();
+            jane.print();
+        }
     }
-}
+    
